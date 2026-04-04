29ad643a582e384098dd4662278bb82a4fe7e8dd
Nguyen Dat <tymmin146@gmail.com>
2026-04-04 14:58:30 +0700

    update product post

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
+const multer = require("multer")
+const cloudinaryHelper = require("../../helper/cloudinary.helper")
+const upload = multer({
+    storage:cloudinaryHelper.storage
+})
 const productController = require("../../controllers/admin/product.controller");
 router.get("/",productController.getProducts);
-router.post("/",productController.createProduct);
+router.post("/",upload.any(),productController.createProduct);
 router.get("/:id",productController.getProductById)
 router.patch("/:id",productController.updateProduct)
 router.delete("/:id",productController.deleteProduct)
