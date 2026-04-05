80dc33c9225eb221bc2d8ee635826a11685b0a32
Khuatlieu <khuatlieu27@gmail.com>
2026-04-05 22:28:30 +0700

    fix

--- routes/admin/pet.route.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router()
-const petController = require("../../controllers/pet.controller")
-router.post("/create",petController.createPost);
-router.patch("/edit/:id",petController.editPatch) 
-
-module.exports = router
