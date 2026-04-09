6b7067581909673cd5f471679a4f23cce3f413f9
Khuatlieu <khuatlieu27@gmail.com>
2026-04-09 16:05:42 +0700

    role + account noibo

---
+++ src/config/permission.js
@@ -0,0 +1,156 @@
+module.exports.permission = [
+    {
+        lable:"Xem trang tổng quan",
+        value:"dashboard-view"
+    },
+    {
+        lable:"Xem thống kê",
+        value:"statistics-view"
+    },
+    {
+        lable:"Xem danh mục",
+        value:"category-view"
+    },
+    {
+        lable:"Tạo danh mục",
+        value:"category-create"
+    },
+    {
+        lable:"Sửa danh mục",
+        value:"category-edit"
+    },
+    {
+        lable:"Xóa danh mục",
+        value:"category-delete"
+    },
+    {
+        lable:"Xem sản phẩm",
+        value:"product-view"
+    },
+    {
+        lable:"Tạo sản phẩm",
+        value:"product-create"
+    },
+    {
+        lable:"Sửa sản phẩm",
+        value:"product-edit"
+    },
+    {
+        lable:"Xóa sản phẩm",
+        value:"product-delete"
+    },
+    {
+        lable:"Xem khách hàng",
+        value:"user-view"
+    },
+    {
+        lable:"Sửa khách hàng",
+        value:"user-edit"
+    },
+    {
+        lable:"Xóa khách hàng",
+        value:"user-delete"
+    },
+    {
+        lable:"Thêm sự kiện cho sản phẩm",
+        value:"product-event"
+    },
+    {
+        lable:"Thùng rác sản phẩm",
+        value:"product-trash"
+    },
+    { lable: "Xem bộ sưu tập", value: "collection-view" },
+    { lable: "Tạo bộ sưu tập", value: "collection-create" },
+    { lable: "Sửa bộ sưu tập", value: "collection-edit" },
+    { lable: "Xóa bộ sưu tập", value: "collection-delete" },
+    { lable: "Xem my designs", value: "my-designs-view" },
+    { lable: "Tạo my designs", value: "my-designs-create" },
+    { lable: "Sửa my designs", value: "my-designs-edit" },
+    { lable: "Xóa my designs", value: "my-designs-delete" },
+    { lable: "Xem thống kê wishlist", value: "wishlist-view" },
+
+    {
+        lable:"Xem đơn hàng",
+        value:"order-view"
+    },
+    {
+        lable:"Sửa đơn hàng",
+        value:"order-edit"
+    },
+    {
+        lable:"Xóa đơn hàng",
+        value:"order-delete"
+    },
+    {
+        lable:"Xem cài đặt",
+        value:"setting-view"
+    },
+    {
+        lable:"Xem thông tin website",
+        value:"info-view"
+    },
+    {
+        lable:"Xem tài khoản nội bộ",
+        value:"account-view"
+    },
+    {
+        lable:"Thêm tài khoản nội bộ",
+        value:"account-create"
+    },
+    {
+        lable:"Sửa tài khoản nội bộ",
+        value:"account-edit"
+    },
+    {
+        lable:"Xóa tài khoản nội bộ",
+        value:"account-delete"
+    },
+    {
+        lable:"Xem nhóm quyền",
+        value:"role-view"
+    },
+    {
+        lable:"Thêm nhóm quyền",
+        value:"role-create"
+    },
+    {
+        lable:"Sửa nhóm quyền",
+        value:"role-edit"
+    },
+    {
+        lable:"Xóa nhóm quyền",
+        value:"role-delete"
+    },
+    {
+        lable:"Xem bài viết",
+        value:"new-view"
+    },
+    {
+        lable:"Thêm bài viết",
+        value:"new-create"
+    },
+    {
+        lable:"Sửa bài viết",
+        value:"new-edit"
+    },
+    {
+        lable:"Xóa bài viết",
+        value:"new-delete"
+    },
+    {
+        lable:"Xem sự kiện",
+        value:"event-view"
+    },
+    {
+        lable:"Tạo sự kiện",
+        value:"event-create"
+    },
+    {
+        lable:"Sửa sự kiện",
+        value:"event-edit"
+    },
+    {
+        lable:"Xóa sự kiện",
+        value:"event-delete"
+    },
+]
