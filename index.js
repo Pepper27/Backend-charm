25952d30fc421793dca6335347a42e5f6e4103c1
Khuatlieu <khuatlieu27@gmail.com>
2026-04-26 10:23:14 +0700

    fix router

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { connectDB } = require("./src/config/database.js");
 const apiRoutes = require("./src/routes/index.route.js");
 
 const app = express();
-const port = process.env.PORT || 3866;
+const port = process.env.PORT || 3861;
 app.set("trust proxy", 1);
 
 const start = async () => {
