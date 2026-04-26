4cbc78493b8cee2182a182a9113c2eae36cf6cf3
Nguyen Dat <tymmin146@gmail.com>
2026-04-26 20:12:12 +0700

    color material be

--- src/routes/legacy/admin/color.route.js
+++ src/routes/legacy/admin/color.route.js
@@ -1,10 +1,28 @@
 const router = require("express").Router();
 const authMiddleware = require("../../../middlewares/admin/auth.middleware");
 const Color = require("../../../models/color.model");
-
+const AccountAdmin = require("../../../models/accountAdmin.model");
+const moment = require("moment");
 router.get("/", authMiddleware.verifyToken, async (req, res) => {
 	try {
 		const list = await Color.find({ deleted: false }).lean();
+		for (const item of list) {
+			if (item.createdBy) {
+				const createdByName = await AccountAdmin.findOne({
+				_id: item.createdBy,
+				});
+				item.createdByName = createdByName?.fullName;
+			}
+			if (item.updatedBy) {
+				const updatedByName = await AccountAdmin.findOne({
+				_id: item.updatedBy,
+				});
+				item.updatedByName = updatedByName?.fullName;
+			}
+			item.createdAtFormat = moment(item.createdAt).format("HH:mm - DD/MM/YYYY");
+			item.updatedAtFormat = moment(item.updatedAt).format("HH:mm - DD/MM/YYYY");
+		}
+				
 		res.status(200).json({ data: list });
 	} catch (err) {
 		res.status(500).json({ message: "Lỗi server" });
@@ -23,6 +41,8 @@ router.get("/:id", authMiddleware.verifyToken, async (req, res) => {
 
 router.post("/", authMiddleware.verifyToken, async (req, res) => {
 	try {
+		req.body.createdBy = req.account.id;
+		req.body.updatedBy = req.account.id;
 		const m = new Color(req.body);
 		await m.save();
 		res.status(200).json({ code: "success" });
@@ -33,6 +53,7 @@ router.post("/", authMiddleware.verifyToken, async (req, res) => {
 
 router.patch("/:id", authMiddleware.verifyToken, async (req, res) => {
 	try {
+		req.body.updatedBy = req.account.id;
 		await Color.updateOne({ _id: req.params.id }, req.body);
 		res.status(200).json({ code: "success" });
 	} catch (err) {
